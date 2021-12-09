1199b1ec15f4722816e5c60e4e32c8f0dd10ced0
Pablo Rojas Fernandez <pablo.rojas.fernandez@alumnos.upm.es>
2021-12-09 20:48:23 +0100

    Coverage 90%

---
+++ src/test/java/com/practica/cajablanca/GeneralEditorTest.java
@@ -0,0 +1,83 @@
+package com.practica.cajablanca;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.cajanegra.EmptyCollectionException;
+
+public class GeneralEditorTest {
+	
+	private Editor editor;
+	
+	@BeforeEach
+	private void setUp() {
+		editor = new Editor();
+	}
+	
+	@Test
+	public void editIsEmptyTest() {
+		assertTrue(editor.editIsEmpty());
+		editor.leerFichero("vacio.txt");
+		assertFalse(editor.editIsEmpty());
+	}
+	
+	@Test
+	public void getLinealEmptyCollectionException() {
+		assertThrows(EmptyCollectionException.class, () -> {
+			editor.getLinea(1);
+		});
+	}
+	
+	@Test
+	public void getLinealIllegalArgumentExceptionMenorCero() {
+		editor.leerFichero("vacio.txt");
+		assertThrows(IllegalArgumentException.class, () -> {
+			editor.getLinea(-1);
+		});
+	}
+	
+	@Test
+	public void getLinealIllegalArgumentExceptionMayorSize() {
+		editor.leerFichero("vacio.txt");
+		assertThrows(IllegalArgumentException.class, () -> {
+			editor.getLinea(editor.size()+1);
+		});
+	}
+	
+//	@Test
+//	public void leerFicheroException() throws Exception {
+//		editor.leerFichero("a");
+//	}
+	
+	@Test
+	public void numPalabrasEmptyCollectionException() {
+		assertThrows(EmptyCollectionException.class, () -> {
+			editor.numPalabras();
+		});
+	}
+	
+	@Test
+	public void numPalabraVacio() throws EmptyCollectionException {
+		editor.leerFichero("vacio.txt");
+		try {
+			editor.numPalabras();
+		} catch (EmptyCollectionException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void numPalabra() throws EmptyCollectionException {
+		editor.leerFichero("avion.txt");
+		try {
+			editor.numPalabras();
+		} catch (EmptyCollectionException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
